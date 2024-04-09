6fe0b617c16837bb2f2247621a274cfe30623713
anshkie <anshgup444@gmail.com>
2024-04-10 00:19:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from './component/Card';
+import CardsData from './component/CardData';
+import CardList from './component/CardList';
+import DemoComponent from './component/DemoComponent';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Home from './component/Home';
+import Navi from './component/Navi';
+import PaginatedItems from './component/PaginatedItems';
+import Form from './NextPage/Form';
+import NextPage from './NextPage/NextPage';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-10 App">
+        <Navi/>     
+        <Header/>
+        <Home/>
+        <div className="flex justify-center">
+      
+      <div className="w-4"></div> {/* Space between cards */}
+      <PaginatedItems data={CardsData} itemsPerPage={12}  // Define your card data here}
+/>
+    </div>
+    
+    <NextPage/>
+    <Footer/>
     </div>
   );
 }
